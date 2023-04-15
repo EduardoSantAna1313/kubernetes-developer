410f3174d81541681d893b616da1cf1a61b92faf
Eduardo <eduardosantana1313@gmail.com>
2023-04-14 21:55:45 -0300

    Adding commands with flag --record to show the change-cause on history rollout command

--- 03-rollout/commands.sh
+++ 03-rollout/commands.sh
@@ -8,6 +8,9 @@ printf ">>>List deployments\n"
 
 kubectl get deploy 
 
+printf "\n>>>Sleeping for 10s<<<\n"
+sleep 10
+
 printf "\n>>>Get pods\n"
 
 kubectl get pods
@@ -24,21 +27,45 @@ printf "\n>>>Show history\n"
 
 kubectl rollout history deployment nginx-deployment
 
+printf "\n>>>Change the replicas\n"
+
+kubectl scale deployment nginx-deployment --replicas=1 --record
+
+printf "\n>>>Sleeping for 5s<<<"
+
+sleep 5
+
+printf "\n>>>Get pods after scale\n"
+
+kubectl get pods
+
+printf "\n>>>Set the image\n"
+
+kubectl set image deployment nginx-deployment nginx=nginx:1.18 --record
+
+printf "\n>>>Describe deploy image\n"
+
+kubectl describe deployment nginx-deployment | grep "Image"
+
 printf "\n>>>Restarting deployment\n"
 
 kubectl rollout restart deployment nginx-deployment
+
+printf "\n>>>Listing pods after restart deployment\n"
+
+kubectl get pods
 
 printf "\n>>>Getting the rollout status\n"
 
 kubectl rollout status deployment nginx-deployment
 
-printf "\nListing pods after restart deployment\n"
-
-kubectl get pods
-
 printf "\n>>>Show history after the restart\n"
 
 kubectl rollout history deployment nginx-deployment
+
+printf "\n>>>Last get all\n\n"
+
+kubectl get all
 
 printf "\n>>>Delete deployment\n"
 
