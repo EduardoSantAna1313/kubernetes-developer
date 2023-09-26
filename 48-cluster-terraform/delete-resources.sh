5e39614849ef71af20ae663a5fd89c05d93caaf7
Eduardo <eduardosantana1313@gmail.com>
2023-09-25 22:24:45 -0300

    Correção script delete resources

--- 48-cluster-terraform/delete-resources.sh
+++ 48-cluster-terraform/delete-resources.sh
@@ -5,7 +5,7 @@ fi
 
 if [[ $(kubectl get ns external-secrets --no-headers | wc -l) == 1 ]]; then
     printf "### Deleting namespace external-secrets ###\n"
-    kubectl delete ns istio-system --force --grace-period=0
+    kubectl delete ns external-secrets --force --grace-period=0
 fi
 
 if [[ $(kubectl get ns istio-system --no-headers | wc -l) == 1 ]]; then
